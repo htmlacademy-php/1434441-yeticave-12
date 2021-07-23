86fa43179de91f675bd9fb5b57821f9cdec75da2
Ivan <sorokkahtml@gmail.com>
2021-07-23 21:46:27 +0300

    Добавит отображение цены

--- index.php
+++ index.php
@@ -124,7 +124,7 @@ $adverts = [
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost">10 999<b class="rub">р</b></span>
+                                    <span class="lot__cost"><?=$advert["price"];?><b class="rub">р</b></span>
                                 </div>
                                 <div class="lot__timer timer">
                                     12:23
