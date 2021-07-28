8e622c661e999507ada28e67f99d45b9127d9fc4
Ivan <sorokkahtml@gmail.com>
2021-07-28 21:53:54 +0300

    Подключит файл helpers.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require_once('helpers.php');
+
 $is_auth = rand(0, 1);
 $user_name = "Иван";
 $categories = [
@@ -61,29 +63,6 @@ function get_price($price) {
     $price = number_format($price, 0, ""," ");
     return "$price ₽";
 };
-
-/**
- * Подключает шаблон, передает туда данные и возвращает итоговый HTML контент
- * @param string $name Путь к файлу шаблона относительно папки templates
- * @param array $data Ассоциативный массив с данными для шаблона
- * @return string Итоговый HTML
- */
-function include_template($name, array $data = []) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!is_readable($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-}
 ?>
 
 <?php
